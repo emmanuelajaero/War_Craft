6e7bb1d8e32ebae0ad0ca69fd35766e5d300fae7
emmanuel ajaero <emmanuelajaero.ea@gmail.com>
2024-02-06 23:38:03 -0500

    explosion sprite

---
+++ src/explosion.py
@@ -0,0 +1,22 @@
+# explosion.py
+
+import pygame
+from .constants import *
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.image_ori = pygame.image.load("assets/images/explosion-32.png").convert()
+        self.image_ori.set_colorkey(WHITE)
+        self.image = self.image_ori.copy()
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.x
+        self.rect.centery = self.y
+        self.last_time = pygame.time.get_ticks()
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_time > 200:
+            self.kill()
