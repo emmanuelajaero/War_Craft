95e13698e0253633ff7e4b4210caa683f55967f0
emmanuel ajaero <emmanuelajaero.ea@gmail.com>
2024-02-06 23:38:26 -0500

    player sprite

---
+++ src/player.py
@@ -0,0 +1,37 @@
+# player.py
+
+import pygame
+
+from .bullet import Bullet
+from .constants import *
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #create a screen
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.image = pygame.image.load("assets/images/player.png").convert()
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.speedx = -8
+        if keys[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self, sprites, bullets):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        sprites.add(bullet)
+        bullets.add(bullet)
