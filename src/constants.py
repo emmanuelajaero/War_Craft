d9d218fec22cfd2ec307a906d88c9449aa7ba3e3
emmanuel ajaero <emmanuelajaero.ea@gmail.com>
2024-02-06 23:38:47 -0500

    constants

---
+++ src/constants.py
@@ -0,0 +1,31 @@
+
+WIDTH = 800         # the width of the game screen
+HEIGHT = 600        # the height of the game screen
+FRAME_RATE = 30     # initialize how many frames per second
+ENEMY_SPEED_Y = 2   # this is the number that determine how fast the enemy will approach the player
+ENEMY_SPEED_X = 2   # the speed the enemy moves with in the x axis
+BULLET_SPEED = 10   # the speed of travel of the bullet
+INCREMENT_ENEMY = 1 # how many enemies will be added to the screen
+
+HIGHEST_SCORE = 0   # variable updated from static file at the start of the game and updated at the end of the game with the highest score so far
+CURRENT_LEVEL = 1   # the current level of the game
+CURRENT_SCORE = 0   # the current accumulated score
+STAGE_CHANGE_SCORE = 10 # at what score will the game stage change
+REMAINING_LIFE = 6  # the number of life left for the player
+
+WHITE = (255, 255, 255) # define white color
+BLACK = (0, 0, 0)       # define black color
+RED = (255, 0, 0)       # Define red color
+YELLOW = (255, 255, 0)  # Define yellow color
+
+
+enemy_speed_y = 2 #2     #this is the number that determine how fast the enemy will approach the player
+enemy_speed_x = 2 #2       #the speed the enemy moves with in the x axis
+bullet_speed = 10       #the speed of travel of the bullet
+increment_enemy = 1     #how many enemies will be added to the screen
+
+highesrt_score = 0       #variable updated from static file at the start of the game and updated at the end of the game with the highest score so far
+current_level = 1       #the current level of the game
+current_score = 0       #the current accumulated score
+stage_change_score = 10 #at what score will the game stage change
+remaining_life = 6      #the number of life left for the player
